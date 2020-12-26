82ef71e0bd5f098430ef8efb6704159e6fb67956
Lazarus Sandhagala Francis <s.f.lazarus7@gmail.com>
2020-12-26 03:49:59 -0600

    added data and model

---
+++ app/src/main/java/com/sflazarus/trivia/model/Question.java
@@ -0,0 +1,31 @@
+package com.sflazarus.trivia.model;
+
+public class Question {
+
+    private String answer;
+    private Boolean answerTrue;
+
+    public Question(String answer, Boolean answerTrue) {
+        this.answer = answer;
+        this.answerTrue = answerTrue;
+    }
+
+    public Question() {
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Boolean getAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(Boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
